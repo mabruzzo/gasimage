572a6448d248dfa57f715c0fa5adb3bb7115df61
Matthew Abruzzo <matthewabruzzo@gmail.com>
2023-10-24 13:06:39 -0400

    adding import statements directly to __init__.py

---
+++ gasimage/__init__.py
@@ -0,0 +1,11 @@
+# do some setup so that the user doesn't need to know how modules and
+# datastructures are organized within this package
+
+from gasimage.optically_thin_ppv import (
+    convert_intensity_to_Tb,
+    optically_thin_ppv,
+)
+
+from gasimage.ray_collection import parallel_ray_grid perspective_ray_grid
+
+from gasimage.rt_config import default_spin_flip_props
