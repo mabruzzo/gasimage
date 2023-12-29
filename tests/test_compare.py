phasing in a correct doppler-parameter formula (while preserving the broken legacy option).

tests/test_compare.py
@@ -221,7 +221,7 @@ from debug_plotting import plot_ray_spectrum, plot_rel_err
 
 # We are going to setup 2
 
-def test_wip(indata_dir):
+def test_compare_full_noscatter_rt(indata_dir):
     enzoe_sim_path = os.path.join(
         indata_dir, ('X100_M1.5_HD_CDdftCstr_R56.38_logP3_Res8/cloud_07.5000/'
                      'cloud_07.5000.block_list')
@@ -274,14 +274,14 @@ def test_wip(indata_dir):
 
     assert_allclose_units(
         actual = actual_rslt['integrated_source'],
-        desired = alt_rslt['integrated_source'], rtol = 4e-10, atol=0,
+        desired = alt_rslt['integrated_source'], rtol = 3e-9, atol=0,
         err_msg = ("error occured while comparing the integrated_source with "
                    "the result from a simpler (slower) implementation")
     )
 
     np.testing.assert_allclose(
         actual = actual_rslt['total_tau'], desired = alt_rslt['total_tau'],
-        rtol = 9e-12, atol=0,
+        rtol = 2e-10, atol=0,
         err_msg = ("error occured while comparing the total optical depth with "
                    "the result from a simpler (slower) implementation")
     )
@@ -315,7 +315,7 @@ def _dumber_full_noscatter_rt(accum_strat, concrete_ray_list, ds):
                 uvec[1] * ray_data['gas','velocity_y'] +
                 uvec[2] * ray_data['gas','velocity_z']).to('cm/s')
 
-        if True: # this is the older "wrong" behavior
+        if False: # this is the older "legacy" behavior (it's wrong!)
             doppler_parameter_b = np.sqrt(
                 2.0 * unyt.kboltz_cgs * ray_data['gas','temperature'] /
                 (ray_data['gas','mean_molecular_weight'] * unyt.mh_cgs)
@@ -341,23 +341,3 @@ def _dumber_full_noscatter_rt(accum_strat, concrete_ray_list, ds):
 
     return {'integrated_source' : integrated_source,
             'total_tau' : integrated_tau}
-
-def full_rt(accum_strat, concrete_ray_list, ds):
-    pass
-
-
-# we can write 2 sets of tests:
-#  - First, we can perform RT on the outputs of collect_ray_values
-#  - Second, we can compare the accuracy of
-#    collect_ray_values(ds, start_end_pairs, fields) and our traverse grid
-#    function
-
-
-
-
-
-# perform a comparison of 3 or 4 function calls
-# -> use result of collect_ray_values and do all RT in one-shot
-# -> use result of collect_ray_values and check that result is consistent when
-#    we subdivide the region of rt
-# -> use the full-blown API

tests/test_full.py
@@ -3,11 +3,15 @@ import os
 
 import numpy as np
 import unyt
+import yt
 
-from gasimage.optically_thin_ppv import convert_intensity_to_Tb
+from gasimage.optically_thin_ppv import (
+    convert_intensity_to_Tb,
+    optically_thin_ppv
+)
+from gasimage.ray_collection import perspective_ray_grid
 from gasimage.snapdsinit import SnapDatasetInitializer
 from gasimage.utils.fits import write_to_fits, read_cube_from_fits
-from gasimage.utils.generate_image import generate_image_arr
 from gasimage.utils.testing import assert_allclose_units
 
 _REF_REST_FREQ = np.float32(1.4204058E+09)*unyt.Hz
@@ -47,6 +51,133 @@ def _dummy_create_field_callback(ds, use_trident_ion = False):
                      sampling_type = 'local', units = 'auto', take_log = True,
                      dimensions = unyt.dimensions.number_density)
 
+def generate_image_arr(ds_initializer, v_channels, sky_delta_latitude_arr_deg,
+                       sky_longitude_arr_deg, obs_distance,
+                       sky_latitude_ref_deg = 0.0,
+                       domain_theta_rad = np.pi/2, domain_phi_rad = 3*np.pi/2,
+                       ndens_HI_n1state = ('gas', 'H_p0_number_density'),
+                       doppler_parameter_b = None,
+                       use_cython_gen_spec = True,
+                       force_general_consolidation = False,
+                       rescale_length_factor = None, pool = None):
+    """
+    THIS IS FOR TESTING PURPOSES - this was a legacy function written a long
+    time ago! We could probably delete this function!
+
+    Generate a mock ppv image of a simulation using a ray-tracing radiative
+    transfer algorithm that assumes that the gas is optically thin.
+
+    The resulting image has a pixel for each combination of `v_channel`, 
+    `sky_delta_latitude_deg`, and `sky_longitude_arr_deg`.
+
+    Parameters
+    ----------
+    ds_initializer:
+         This can either be a yt-dataset or a dataset initialization object.
+         The latter is required if you want to use a parallel pool (since you
+         can't pickle these datasets)
+    rescale_length_factor: float, optional
+        When specified, the width of each cell in the simulation is multiplied 
+        by this factor. (This effectively holds the position of the 
+        simulation's origin fixed in place).
+    v_channels: 1D `unyt.unyt_array`
+        A monotonically increasing array of velocity channels.
+    sky_delta_latitude_arr_deg: 1D np.ndarray
+        Array of sky-latitude locations (in degrees), where rays should be 
+        traced. These are measured with respect to the sky-latitude of the ray
+        between the observer and the reference point, from the observer's
+        perspective. That reference sky-latitude is specified by 
+        sky_latitude_ref_deg.
+    sky_longitude_arr_deg: 1D np.ndarray
+        Array of sky-longitude locations (in degrees), where rays should be 
+        traced. For reference, the sky-longitude of the ray between the
+        observer and the reference point is always assumed to be zero because
+        it has no effect on the resulting mercator projection and can be 
+        arbitrarily changed later.
+    obs_distance : unyt.unyt_quantity
+        Distance between the observer and the reference point (which currently 
+        coincides with the center of the simulation domain).
+    sky_latitude_ref_deg: float, optional
+        specify the latitude on the sky (from the observer's perspective) that 
+        the primary ray passes through. This can have significant implications 
+        for the mercator projection. The default value is 0.0 (corresponding to
+        the celestial equator). 
+    domain_theta_rad, domain_phi_rad: float
+        Alongside obs_distance, these quantities specify the location of the 
+        observer in spherical coordinates relative to the reference point
+        (which is currently assumed to coincide with the center of the
+        simulation domain). Note that the spherical coordinates assuming that 
+        the x,y, and z directions align with the simulation's native coordinate
+        axes (for reference, the x,y,z directions in the observer's frame are
+        generally different).
+    ndens_HI_n1state
+        The name of the yt-field holding the number density of H I atoms
+        (neutral Hydrogen) in the electronic ground state (i.e. the electron is
+        in the n=1 orbital). See the documentation for `optically_thin_ppv` for
+        more discussion about the motivation for the default value.
+    rescale_length_factor: float, Optional
+        When not `None`, the width of each cell is multiplied by this factor.
+        (This effectively holds the position of the simulation's origin fixed
+        in place).
+    pool: Optional
+        A taskpool from the schwimmbad package can be specified for this 
+        argument to facillitate parallelization.
+
+    Notes
+    -----
+    Presently, this function assumes that the "reference point" coincides with
+    the center of the simulation. It might be nice to use an arbitrary
+    reference point instead (all of the helper functions should already support
+    this, but this should be tested). If that is done, some decisions will need
+    to be made in order to avoid ambiguities with 
+
+    `rescale_length_factor` is provided for rescaling the sizes of adiabatic
+    simulations (since they are scale-free).
+    """
+    # produce a mock image (not initially in terms of brightness temperature)
+    if isinstance(ds_initializer, yt.data_objects.static_output.Dataset):
+        _ds = ds_initializer
+        assert pool is None # can't parallelize!
+    else:
+        _ds = ds_initializer()
+
+    if not (-90.0 < sky_latitude_ref_deg < 90.0):
+        # testing will need to be required when it's equal to 90.0
+        raise ValueError("sky_latitude_ref_deg must be between -90.0 & + 90.0")
+    else:
+        assert (sky_latitude_ref_deg + sky_delta_latitude_arr_deg.min()) > -90.0
+        assert (sky_latitude_ref_deg + sky_delta_latitude_arr_deg.max()) < 90.0
+
+    if sky_latitude_ref_deg != 0.0:
+        raise ValueError("the sky_latitude_ref_deg argument is untested with "
+                         "a value other than 0.0")
+
+    if rescale_length_factor is not None:
+        assert rescale_length_factor > 0 and np.ndim(rescale_length_factor) == 0
+
+    # determine the starting and ending points of all rays
+    ray_collection = perspective_ray_grid(
+        sky_latitude_ref_deg = sky_latitude_ref_deg,
+        observer_distance = obs_distance,
+        sky_delta_latitude_arr_deg = sky_delta_latitude_arr_deg,
+        sky_longitude_arr_deg = sky_longitude_arr_deg,
+        domain_theta_rad = domain_theta_rad,
+        domain_phi_rad = domain_phi_rad,
+        ds = _ds, rescale_length_factor = rescale_length_factor
+    )
+
+    out = optically_thin_ppv(
+        v_channels, ray_collection = ray_collection, ds = ds_initializer,
+        ndens_HI_n1state = ndens_HI_n1state,
+        doppler_parameter_b = doppler_parameter_b,
+        use_cython_gen_spec = use_cython_gen_spec,
+        rescale_length_factor = rescale_length_factor,
+        force_general_consolidation = force_general_consolidation,
+        pool = pool
+    )
+    return out
+
+
 def _create_raw_ppv(enzoe_sim_path,
                     sky_delta_latitude_arr_deg,
                     sky_longitude_arr_deg, v_channels,
@@ -55,8 +186,12 @@ def _create_raw_ppv(enzoe_sim_path,
                     domain_theta_rad = np.pi/2,
                     domain_phi_rad = 3*np.pi/2,
                     use_cython_gen_spec = True,
-                    force_general_consolidation = False, nproc = 1):
-    # before use_cython_gen_spec existed, it was effectively False
+                    doppler_parameter_b = None,
+                    force_general_consolidation = False,
+                    nproc = 1):
+    # -> before use_cython_gen_spec existed, it was effectively False
+    # -> to get the behavior equivalent to before doppler_parameter_b was
+    #    a parameter of this function, pass it 'legacy'
     ds_loader = SnapDatasetInitializer(
         enzoe_sim_path, setup_func = _dummy_create_field_callback)
 
@@ -78,6 +213,7 @@ def _create_raw_ppv(enzoe_sim_path,
             ndens_HI_n1state = ('gas', 'H_p0_number_density'),
             rescale_length_factor = 1,
             use_cython_gen_spec = use_cython_gen_spec,
+            doppler_parameter_b = doppler_parameter_b,
             force_general_consolidation = force_general_consolidation,
             pool = pool
         )
@@ -96,8 +232,11 @@ def _create_raw_ppv_and_save_fits(enzoe_sim_path, out_fname,
                                   domain_theta_rad = np.pi/2,
                                   domain_phi_rad = 3*np.pi/2,
                                   use_cython_gen_spec = True,
+                                  doppler_parameter_b = None,
                                   clobber_file = False, nproc = 1):
-    # before use_cython_gen_spec existed, it was effectively False
+    # -> before use_cython_gen_spec existed, it was effectively False
+    # -> to get the behavior equivalent to before doppler_parameter_b was
+    #    a parameter of this function, pass it 'legacy'
 
     _, ppv_Tb = _create_raw_ppv(
         enzoe_sim_path = enzoe_sim_path,
@@ -108,6 +247,7 @@ def _create_raw_ppv_and_save_fits(enzoe_sim_path, out_fname,
         sky_latitude_ref_deg = sky_latitude_ref_deg,
         domain_theta_rad = domain_theta_rad,
         domain_phi_rad = domain_phi_rad,
+        doppler_parameter_b = doppler_parameter_b,
         use_cython_gen_spec = use_cython_gen_spec,
         nproc = 1)
 
@@ -180,6 +320,8 @@ def _test_full_raytrace_perspective_helper(answer_test_config, indata_dir,
         sky_latitude_ref_deg = 0.0,
         domain_theta_rad = np.pi/2, domain_phi_rad = 3*np.pi/2,
         use_cython_gen_spec = True,
+        doppler_parameter_b = 'legacy', # only use this so we don't have to
+                                        # change our test-answers
         nproc = 1
     )
 
@@ -277,6 +419,8 @@ def _test_compare_gen_spec(indata_dir, cython_legacy_comparison,
             obs_distance = unyt.unyt_quantity(12.4, 'kpc'),
             sky_latitude_ref_deg = 0.0,
             domain_theta_rad = np.pi/2, domain_phi_rad = 3*np.pi/2,
+            doppler_parameter_b = 'legacy', # only use this so we don't have to
+                                            # change our test-answers
             nproc = 1, **kwargs
         )
         ppv_arrs.append(ppv)